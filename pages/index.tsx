1f84193241db4087acf6b5a8377d55cf6ab371f4
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2023-06-20 14:57:20 -0400

    learning mantine

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { CreateStylesDemo } from '../components/CreateStylesDemo/CreateStylesDemo';
 
 export default function HomePage() {
   return (
     <>
       <Welcome />
       <ColorSchemeToggle />
+      <CreateStylesDemo />
     </>
   );
 }
