47564426573d0fc2a9332d4cd83483002edc1841
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2023-06-21 08:21:30 -0400

    still working on mantine's global style. Getting the hang of it

--- components/Welcome/Welcome.styles.ts
+++ components/Welcome/Welcome.styles.ts
@@ -1,8 +1,10 @@
 import { createStyles } from '@mantine/core';
 
-export default createStyles((theme) => ({
+export default createStyles((
+  // theme
+  ) => ({
   title: {
-    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    // color: theme.colorScheme === 'dark' ? theme.white : theme.black,
     // fontSize: 100,
     // fontWeight: 900,
     // letterSpacing: -2,
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -39,6 +39,13 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
               lg: '1rem',
               xl: '1.2rem',
             },
+            headings: {
+              sizes: {
+                h1: { fontSize: '2em' },
+                h2: { fontSize: '1.5em' },
+                h3: { fontSize: '1.17em' },
+              },
+            },
             spacing: { xs: '1rem', sm: '1.2rem', md: '1.8rem', lg: '2.2rem', xl: '2.8rem' },
             activeStyles: { transform: 'scale(0.95)' },
             defaultGradient: { from: 'orange', to: 'red', deg: 45 },
@@ -50,8 +57,52 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
               body: {
                 ...theme.fn.fontStyles(),
                 backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
-                color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+                // color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
                 lineHeight: theme.lineHeight,
+              },
+              h1: {
+                fontSize: '2em',
+              },
+              h2: {
+                fontSize: '1.5em',
+              },
+              h3: {
+                fontSize: '1.17em',
+              },
+              '@media (max-width: 640px)': {
+                body: {
+                  fontSize: theme.fontSizes.xs,
+                  fontWeight: 200,
+                  padding: theme.spacing.xs,
+                },
+              },
+              '@media (min-width: 640px)': {
+                body: {
+                  fontSize: theme.fontSizes.sm,
+                  fontWeight: 300,
+                  padding: theme.spacing.sm,
+                },
+              },
+              '@media (min-width: 768px)': {
+                body: {
+                  fontSize: theme.fontSizes.md,
+                  fontWeight: 400,
+                  padding: theme.spacing.md,
+                },
+              },
+              '@media (min-width: 1024px)': {
+                body: {
+                  fontSize: theme.fontSizes.lg,
+                  fontWeight: 500,
+                  padding: theme.spacing.lg,
+                },
+              },
+              '@media (min-width: 1280px)': {
+                body: {
+                  fontSize: theme.fontSizes.xl,
+                  fontWeight: 600,
+                  padding: theme.spacing.xl,
+                },
               },
               // '.your-class': {
               //   backgroundColor: 'red',
