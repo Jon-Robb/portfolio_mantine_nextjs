36f21ffe68ad9c1530ea060e16c1413bca6e6e9a
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2023-06-23 13:18:53 -0400

    added mantine's select

--- components/AppNavMenu/AppNavMenu.tsx
+++ components/AppNavMenu/AppNavMenu.tsx
@@ -1,6 +1,7 @@
-import { Navbar, Anchor, Text } from '@mantine/core';
+import { Navbar, Anchor, Text, Select } from '@mantine/core';
 import NavLinkMenu from '../NavBarNavLinkMenu/NavBarNavLinkMenu';
 import IconsContainer from '../IconsContainer/IconsContainer';
+import { ColorSchemeToggle } from '../ColorSchemeToggle/ColorSchemeToggle';
 import useStyles from './AppNavMenu.styles';
 
 interface AppNavbarProps {
@@ -13,6 +14,15 @@ export default function AppNavMenu({ opened }: AppNavbarProps) {
     <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 250, md: 275, lg: 300 }} className={classes.fullNavbar}>
       <div className={classes.navMenuContainer}>
         <Text className={classes.titleName}> <Anchor href="/"> Jonathan Robinson </Anchor> </Text>
+        <ColorSchemeToggle />
+        <Select
+          data={[
+            { value: 'en', label: 'English' },
+            { value: 'fr', label: 'Français' },
+            { value: 'es', label: 'Español' },
+          ]}
+          defaultValue="en"
+        />
         <NavLinkMenu />
         <IconsContainer />
       </div>
