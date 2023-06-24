42b3e54ab0b0cff6529fb1819a3f61d7d51416c2
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2023-06-24 13:34:49 -0400

    working on badges gradients

--- components/SkillBadge/SkillBadge.tsx
+++ components/SkillBadge/SkillBadge.tsx
@@ -1,29 +1,55 @@
 import { useState } from 'react';
+import { useMediaQuery } from '@mantine/hooks';
 import { Badge } from '@mantine/core';
 import { AnimateCssKeys } from '../../animation/AnimateCssKeys';
 
-export default function SkillBadge() {
-    const [animation, setAnimation] = useState('');
-    const animations: string[] = AnimateCssKeys;
+interface SkillBadgeProps {
+  label: string;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+  color?: string;
+  gradient?: boolean;
+  animated?: boolean;
+  gradientColors?: string[];
+}
 
-    const getRandomAnimation = () => {
-        const randomIndex = Math.floor(Math.random() * animations.length);
-        return AnimateCssKeys[randomIndex];
-    };
+export default function SkillBadge({
+  label,
+  leftIcon,
+  rightIcon,
+  color,
+  gradient,
+  animated = true,
+  gradientColors }:
+  SkillBadgeProps) {
+  const [animation, setAnimation] = useState('');
+  const animations: string[] = AnimateCssKeys;
+  const isMobile = useMediaQuery('(max-width: 768px)');
 
-    const handleMouseEnter = () => {
-        setAnimation('');
-        const newAnimation = getRandomAnimation();
-        setAnimation(`animate__${newAnimation}`);
-    };
+  const getRandomAnimation = () => {
+    const randomIndex = Math.floor(Math.random() * animations.length);
+    return AnimateCssKeys[randomIndex];
+  };
 
-    return (
-        <Badge
-          className={`animate__animated ${animation}`}
-          onMouseEnter={handleMouseEnter}
-          style={{ cursor: 'wait' }}
-        >
-          I&apos;m animated!
-        </Badge>
-    );
+  const handleAnimation = () => {
+    setAnimation('');
+    const newAnimation = getRandomAnimation();
+    setAnimation(`animate__${newAnimation}`);
+  };
+
+  return (
+    <Badge
+      className={`animate__animated ${animation}`}
+      onMouseEnter={isMobile && animated ? undefined : handleAnimation}
+      onClick={isMobile && animated ? handleAnimation : undefined}
+      style={{ cursor: 'wait' }}
+      leftSection={leftIcon}
+      rightSection={rightIcon}
+      color={color}
+      variant={gradient ? 'gradient' : 'filled'}
+      // gradient={{ from: gradientColors[0] ?? '', to: gradientColors[1] ?? '' }}
+    >
+      {label}
+    </Badge>
+  );
 }
