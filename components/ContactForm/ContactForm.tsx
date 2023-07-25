6b6f8f341d5e25c6d81bb8570979864cd8babe59
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2023-07-25 13:18:28 -0400

    looking for mail sender API

--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -6,27 +6,28 @@ export default function ContactForm() {
     const { classes } = useStyles();
     const form = useContactForm();
 
+    const handleSubmit = (values:any) => {
+        console.log(values);
+    };
+
     return (
         <Paper className={classes.wrapper}>
-            <form onSubmit={form.onSubmit((values) => console.log(values))}>
+            <form onSubmit={handleSubmit}>
                 <TextInput
                   label="Name"
                   placeholder="Enter your name"
-                  required
                   withAsterisk
                   {...form.getInputProps('name')}
                 />
                 <TextInput
                   label="Email"
                   placeholder="Enter your email"
-                  required
                   withAsterisk
                   {...form.getInputProps('email')}
                 />
                 <Textarea
                   label="Message"
                   placeholder="Enter your message"
-                  required
                   withAsterisk
                   {...form.getInputProps('message')}
                 />
