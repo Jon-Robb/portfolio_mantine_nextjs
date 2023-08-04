0e2c3a302dff0828e534d31a3677867dacc5f39b
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2023-08-04 16:26:09 -0400

    cleaned contactform

--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Button, Paper, Notification } from '@mantine/core';
 import useContactForm from '../../hooks/useContactForm';
 import { useScreenSize } from '../../hooks/useScreenSize';
 import useStyles from './ContactForm.styles';
-import { EMessages } from '../../typescript/enums/EMessages';
-import { checkEmail, sendEmail, sendConfirmationMail } from './ContactFormServices';
+import useEmailStatusAndNotification from '../../hooks/useEmailStatusAndNotification';
 import NameInput from './ContactFormGroup/NameInput/NameInput';
 import EmailInput from './ContactFormGroup/EmailInput/EmailInput';
 import TextAreaContactForm from './ContactFormGroup/TextArea/TextAreaContactForm';
@@ -13,60 +12,22 @@ export default function ContactForm() {
     const { classes } = useStyles();
     const form = useContactForm();
     const screenSize = useScreenSize();
-    const [loading, setLoading] = useState(false);
-    const [notification, setNotification] = useState({ title: '', message: '' });
-    const [isVerifiedEmail, setIsVerifiedEmail] = useState(false);
-    const [isPendingToken, setIsPendingToken] = useState(false);
-    const [sendConfirmation, setSendConfirmation] = useState(false);
-
-    const verifyEmail = async () => {
-        const response = await checkEmail(form.values.email);
-        if (response === EMessages.EMAIL_VERIFIED) {
-            setIsPendingToken(false);
-            setIsVerifiedEmail(true);
-            setNotification({ title: 'Success', message: 'Email verified successfully.' });
-        } else if (response === EMessages.EMAIL_PENDING) {
-            setIsPendingToken(true);
-            setNotification({ title: 'Pending link', message: 'Your Email address has a pending verifying token, please check your emails.' });
-        } else if (response === EMessages.EMAIL_NOT_FOUND) {
-            setNotification({ title: 'Please verify you email', message: 'A link will be sent to your address when you press the button' });
-            setSendConfirmation(true);
-        }
-    };
-
-    const handleSubmit = async (values: any) => {
-        setLoading(true);
-        setNotification({ title: 'Loading', message: 'Sending email' });
-        const response = await sendEmail(values);
-        setLoading(false);
-        if (response.status === 200) {
-            form.reset();
-            setNotification({ title: 'Success', message: 'Email sent successfully.' });
-        } else {
-            setNotification({ title: 'Error', message: 'Email failed to send, please try again later.' });
-        }
-    };
-
-    const handleEmailBlur = async () => {
-        if (form.values.email !== '') {
-            form.validateField('email');
-            if (!form.isValid('email')) return;
-            if (!isVerifiedEmail && !isPendingToken) {
-                verifyEmail();
-            }
-        }
-    };
-
-    const handleSendConfirmation = async () => {
-        setSendConfirmation(false);
-        const response = await sendConfirmationMail(form.values.email);
-        if (response === EMessages.EMAIL_SENT) {
-            setNotification({ title: 'Link sent...', message: 'Please click the link in your emails...' });
-            setIsPendingToken(true);
-        } else if (response === EMessages.EMAIL_NOT_SENT) {
-            setNotification({ title: 'Error', message: 'The confirmation email could not get through' });
-        }
-    };
+    const {
+        loading,
+        setLoading,
+        notification,
+        setNotification,
+        isVerifiedEmail,
+        setIsVerifiedEmail,
+        isPendingToken,
+        setIsPendingToken,
+        sendConfirmation,
+        setSendConfirmation,
+        verifyEmail,
+        handleSubmit,
+        handleEmailBlur,
+        handleSendConfirmation,
+    } = useEmailStatusAndNotification(form);
 
     useEffect(() => {
         // polling for 5 minutes
---
+++ hooks/useEmailStatusAndNotification.ts
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import { EMessages } from '../typescript/enums/EMessages';
+import { checkEmail, sendEmail, sendConfirmationMail } from '../components/ContactForm/ContactFormServices';
+
+export default function useEmailStatusAndNotification(form:any) {
+    const [loading, setLoading] = useState(false);
+    const [notification, setNotification] = useState({ title: '', message: '' });
+    const [isVerifiedEmail, setIsVerifiedEmail] = useState(false);
+    const [isPendingToken, setIsPendingToken] = useState(false);
+    const [sendConfirmation, setSendConfirmation] = useState(false);
+
+    const verifyEmail = async () => {
+        const response = await checkEmail(form.values.email);
+        if (response === EMessages.EMAIL_VERIFIED) {
+            setIsPendingToken(false);
+            setIsVerifiedEmail(true);
+            setNotification({ title: 'Success', message: 'Email verified successfully.' });
+        } else if (response === EMessages.EMAIL_PENDING) {
+            setIsPendingToken(true);
+            setNotification({ title: 'Pending link', message: 'Your Email address has a pending verifying token, please check your emails.' });
+        } else if (response === EMessages.EMAIL_NOT_FOUND) {
+            setNotification({ title: 'Please verify you email', message: 'A link will be sent to your address when you press the button' });
+            setSendConfirmation(true);
+        }
+    };
+
+    const handleSubmit = async (values: any) => {
+        setLoading(true);
+        setNotification({ title: 'Loading', message: 'Sending email' });
+        const response = await sendEmail(values);
+        setLoading(false);
+        if (response.status === 200) {
+            form.reset();
+            setNotification({ title: 'Success', message: 'Email sent successfully.' });
+        } else {
+            setNotification({ title: 'Error', message: 'Email failed to send, please try again later.' });
+        }
+    };
+
+    const handleEmailBlur = async () => {
+        if (form.values.email !== '') {
+            form.validateField('email');
+            if (!form.isValid('email')) return;
+            if (!isVerifiedEmail && !isPendingToken) {
+                verifyEmail();
+            }
+        }
+    };
+
+    const handleSendConfirmation = async () => {
+        setSendConfirmation(false);
+        const response = await sendConfirmationMail(form.values.email);
+        if (response === EMessages.EMAIL_SENT) {
+            setNotification({ title: 'Link sent...', message: 'Please click the link in your emails...' });
+            setIsPendingToken(true);
+        } else if (response === EMessages.EMAIL_NOT_SENT) {
+            setNotification({ title: 'Error', message: 'The confirmation email could not get through' });
+        }
+    };
+
+    return {
+        loading,
+        setLoading,
+        notification,
+        setNotification,
+        isVerifiedEmail,
+        setIsVerifiedEmail,
+        isPendingToken,
+        setIsPendingToken,
+        sendConfirmation,
+        setSendConfirmation,
+        verifyEmail,
+        handleSubmit,
+        handleEmailBlur,
+        handleSendConfirmation,
+    };
+}
