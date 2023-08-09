7afd4ac94cfce44e7e1cf2e688c680ef41e4dceb
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2023-08-09 19:05:34 -0400

    working on fade in entry

--- components/Animation/EntryAnimation/EntryAnimation.tsx
+++ components/Animation/EntryAnimation/EntryAnimation.tsx
@@ -1,17 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import useStyles from './EntryAnimation.styles';
 
 export default function EntryAnimation() {
-    const { classes } = useStyles();
-    const [opacity, setOpacity] = useState(0);
 
-    useEffect(() => {
-        setOpacity(0);
-    }, []);
 
-    return (
-        <div className={classes.fadeInWrapper} style={{ opacity }}>
-            Fade in wrapper
-        </div>
-    );
+
+
+    // this is a working fondue
+
+    // const [opacity, setOpacity] = useState(1);
+    // const { classes } = useStyles();
+
+    // useEffect(() => {
+    //     setOpacity(0);
+    //     console.log('opacity', opacity);
+    // }, [opacity]);
+
+    // return (
+    //     <div className={classes.fadeInWrapper} style={{ opacity }} />
+    // );
 }
