07db8b642ef745f9382a748372b06741cdbfa9b8
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2023-08-22 13:29:25 -0400

    working on which section is visible

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import { Button } from '@mantine/core';
 import { useTranslation } from 'react-i18next';
 import StickyTitle from '../StickyTitle/StickyTitle';
@@ -6,7 +6,7 @@ import { useScreenWidth } from '../../hooks/useScreenSize';
 import ProjectCardGrid from '../ProjectCardGrid/ProjectCardGrid';
 import { ProjectsData } from '../../data/ProjectsData';
 import useStyles from './Projects.styles';
-import useIsInViewport from '../../hooks/useVisibility';
+// import useIsInViewport from '../../hooks/useIsInViewport';
 
 export default function Projects() {
   const { t } = useTranslation();
@@ -14,12 +14,12 @@ export default function Projects() {
   const screenWidth = useScreenWidth();
   const [visibleCount, setVisibleCount] = useState<number>(0);
   const [visibleIncrement, setVisibleIncrement] = useState<number>(1);
-  const ref = useRef<HTMLDivElement>(null);
-  const isInViewport = useIsInViewport(ref); // [0.5] is the threshold, [0] is the default value
+  // const ref = useRef<HTMLDivElement>(null);
+  // const entry = useIsInViewport(ref); // [0.5] is the threshold, [0] is the default value
 
-  useEffect(() => {
-    console.log('isInViewport', isInViewport);
-  }, [isInViewport]);
+  // useEffect(() => {
+  //   console.log('isInViewport', entry);
+  // }, [entry]);
 
   useEffect(() => {
     setVisibleCount(screenWidth > 1184 ? 4 : 2);
@@ -35,7 +35,8 @@ export default function Projects() {
   };
 
   return (
-    <section ref={ref} className="section" id="projects">
+    // put ref={ref} on the section tag to use useIsInViewport
+    <section className="section" id="projects">
       <StickyTitle title={t('projects.title')} />
       <ProjectCardGrid visibleCount={visibleCount} />
       <Button.Group className={classes.buttonGroup}>
---
+++ hooks/useIsInViewport.ts
@@ -0,0 +1,58 @@
+import { useEffect, useState, useMemo } from 'react';
+/**
+ * `useIsInViewport` Hook
+ *
+ * This hook checks if a given DOM element (referenced by `ref`) is within the viewport.
+ * It uses the IntersectionObserver API to monitor changes in the intersection of the target
+ * element with the viewport and returns the latest IntersectionObserverEntry object.
+ *
+ * @param {React.RefObject<HTMLElement>} ref - A React ref object pointing to the DOM element to be observed.
+ *
+ * @returns {IntersectionObserverEntry | null} - The latest IntersectionObserverEntry object if the element is observed,
+ *                                               otherwise returns null.
+ *
+ * @example
+ *
+ * const MyComponent = () => {
+ *   const ref = useRef(null);
+ *   const entry = useIsInViewport(ref);
+ *
+ *   const isInViewport = entry?.isIntersecting;
+ *
+ *   return (
+ *     <div ref={ref}>
+ *       {isInViewport ? 'I am in the viewport!' : 'I am not in the viewport.'}
+ *     </div>
+ *   );
+ * };
+ *
+ * Note: Ensure that the target element is attached to the DOM and has dimensions (i.e., height and width)
+ *       before using this hook to get accurate results.
+ */
+const useIsInViewport = (ref: React.RefObject<HTMLElement>): IntersectionObserverEntry | null => {
+  const [entry, setEntry] = useState<IntersectionObserverEntry | null>(null);
+
+  const observer = useMemo(
+    () =>
+      new IntersectionObserver(([newEntry]) => {
+        setEntry(newEntry);
+      }),
+    []
+  );
+
+  useEffect(() => {
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [ref, observer]);
+
+  return entry;
+};
+
+export default useIsInViewport;
