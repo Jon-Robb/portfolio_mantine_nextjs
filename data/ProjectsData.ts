41f1fcf1952cc067f9440b457bc2e9c73b443f66
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2023-09-18 14:07:49 -0400

    feat:adding more projects in projectdata

--- data/ProjectsData.ts
+++ data/ProjectsData.ts
@@ -11,19 +11,19 @@ export const ProjectsData: ProjectCardProps[] = [
         codeUrl: 'https://www.google.com',
         techs: ['React', 'Typescript', 'Phaser3', 'Colyseus', 'WebSockets', 'TailwindCSS', 'Node', ' ExpressJS'],
         collaborators: [
-            { name: 'Cryptoblivious', githubLink: 'https://github.com/cryptoblivious/' },
+            { name: 'Andrzej Wisniowski', githubLink: 'https://github.com/cryptoblivious/' },
         ],
     },
 
     {
         title: 'Boids',
         description: 'projects.boids.desc',
-        imageUrl: '../../images/textanalyzer_mainmenu.png',
+        imageUrl: '../../images/boids_mainpic.png',
         // videoSrc: '../../videos/test.mp4',
         codeUrl: 'https://github.com/Jon-Robb/Boids',
         techs: ['Python', 'Tkinter'],
         collaborators: [
-            { name: 'Cryptoblivious', githubLink: 'https://github.com/cryptoblivious/' },
+            { name: 'Andrzej Wisniowski', githubLink: 'https://github.com/cryptoblivious/' },
             { name: 'Alexis Provost', githubLink: 'https://github.com/alexisprovost/' },
             { name: 'Thomas Pelletier', githubLink: 'https://github.com/Thomkiller/' },
 
@@ -38,11 +38,65 @@ export const ProjectsData: ProjectCardProps[] = [
         techs: ['PHP', 'JavaScript', 'CSS', 'HTML', 'PostgreSQL', 'Ajax'],
     },
     {
-        title: 'Text Analyzer - Synonyms finder',
+        title: 'projects.textanalyzer.title',
         description: 'projects.textanalyzer.desc',
         imageUrl: '../../images/textanalyzer_mainmenu.png',
         // videoSrc: '../../videos/test.mp4',
         codeUrl: 'https://github.com/Jon-Robb/text_analyzer_synonyms_finder',
         techs: ['Python', 'Numpy', 'CLI'],
+        collaborators: [
+            { name: 'Andrzej Wisniowski', githubLink: 'https://github.com/cryptoblivious/' },
+        ],
     },
+    {
+        title: 'projects.fsm.title',
+        description: 'projects.fsm.desc',
+        imageUrl: '../../images/fsm_mainpic.png',
+        codeUrl: 'https://github.com/Jon-Robb/finite_state_machine_library',
+        techs: ['Python'],
+        collaborators: [
+            { name: 'Andrzej Wisniowski', githubLink: 'https://github.com/cryptoblivious/' },
+            { name: 'Nathaelle Fournier', githubLink: 'https://github.com/SakyunBuns' },
+            { name: 'Quoc Huann Tran', githubLink: 'https://github.com/ArsenTigor' },
+        ],
+    },
+    {
+        title: 'projects.ga.title',
+        description: 'projects.ga.desc',
+        imageUrl: '../../images/ga_mainpic.png',
+        // videoSrc: '../../videos/test.mp4',
+        codeUrl: 'https://github.com/Jon-Robb/genetic_algorithm',
+        techs: ['Python', 'Pyside6', 'Numpy', 'Pillow'],
+        collaborators: [
+            { name: 'Andrzej Wisniowski', githubLink: 'https://github.com/cryptoblivious/' },
+            { name: 'Alexis Provost', githubLink: 'https://github.com/alexisprovost/' },
+            { name: 'Thomas Pelletier', githubLink: 'https://github.com/Thomkiller/' },
+        ],
+    },
+    {
+        title: 'Paint',
+        description: 'projects.paint.desc',
+        imageUrl: '../../images/paint_mainpic.png',
+        codeUrl: 'https://github.com/Jon-Robb/simple_paint_android',
+        techs: ['Java', 'Android'],
+    },
+    {
+        title: 'projects.playerspotify.title',
+        description: 'projects.playerspotify.desc',
+        imageUrl: '../../images/playerspotify_mainpic.png',
+        codeUrl: 'https://github.com/Jon-Robb/spotify_app_radiohead',
+        techs: ['Java', 'Android', 'Spotify API'],
+    },
+    {
+        title: 'projects.quizspotify.title',
+        description: 'projects.quizspotify.desc',
+        imageUrl: '../../images/quizspotify_mainpic.png',
+        codeUrl: 'https://github.com/Jon-Robb/spotify_api_quiz_tributetorickgoodness',
+        techs: ['Java', 'Android', 'Spotify API'],
+    },
+    // {
+    //     title:'projects.portfolio.title',
+    //     description:'projects.portfolio.desc',
+
+    // }
 ];
